909a5d5079bc16b608114b24c68135e75178bfe8
Kay Gaaf <kay@vandijk.one>
2026-05-28 18:05:09 +0200

    fix internal wp api urls

--- src/lib/wordpress.ts
+++ src/lib/wordpress.ts
@@ -1,5 +1,4 @@
-const WP_REST_URL = process.env.NEXT_PUBLIC_WORDPRESS_URL || 'https://latticeplugins.com';
-const WP_API_URL = `${WP_REST_URL}/wp-json/wp/v2`;
+const WP_API_URL = process.env.WP_REST_URL || `${process.env.NEXT_PUBLIC_WORDPRESS_URL || 'https://latticeplugins.com'}/wp-json/wp/v2`;
 
 export async function getPosts(per_page = 10) {
   const res = await fetch(`${WP_API_URL}/posts?per_page=${per_page}`, {
